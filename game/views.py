fea725c05b3b5bfa23922254ed72597a512355a6
Lingyuan Li <2210127151@qq.com>
2025-01-28 23:22:25 +0800

    Django-2.1

--- game/views.py
+++ game/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 
 def index(resquest):
-    return HttpResponse("我的第一个网页")
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line2 = '<img src="https://tse2-mm.cn.bing.net/th/id/OIP-C.Y-i9FNrjN1VkUa3fEwX9EwHaE9?w=231&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" width=1000>'
+    return HttpResponse(line1 + line2)
 
 
 
